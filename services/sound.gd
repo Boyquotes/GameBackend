c928dab8b7d1e3b010da6bda08ae80d29248f72f
Sergey Nikitin <medex81@gmail.com>
2023-01-05 01:55:07 +0300

    Исправлена ошибка возвращаемого аргумента для сигнала завершения проигрывания.

--- services/sound.gd
+++ services/sound.gd
@@ -3,7 +3,7 @@ extends Node
 class_name Sounds
 
 const num_players = 10
-var players:Dictionary
+var players:Array[AudioStreamPlayer]
 @onready var player_background = AudioStreamPlayer.new()
 @onready var logs:LoggotLogger = Services.logs
 
@@ -13,26 +13,20 @@ func _ready():
 	for num in num_players:
 		var player = AudioStreamPlayer.new()
 		add_child(player)
-		players[player] = null
-		player.finished.connect(_on_stream_finished)
+		players.append(player)
 	add_child(player_background)
 	
 func _exit_tree():
 	logs.info("Sounds service > exit.")
-		
-func _on_stream_finished(player):
-	players[player] = null
-	
+
 func play(sound_path:String)->bool:
-	if !players.values().has(sound_path):
-		for player in players.keys():
-			if players[player] == null:
-				player.stream = load(sound_path)
-				player.stream.set_loop(false)
-				players[player] = sound_path
-				player.play()
-				logs.info("Scenes service > Play sound " + sound_path)
-				return true
+	for player in players:
+		if !player.playing:
+			player.stream = load(sound_path)
+			player.stream.set_loop(false)
+			player.play()
+			logs.info("Scenes service > Play sound " + sound_path)
+			return true
 	return false
 		
 func play_background(sound_path:String):
